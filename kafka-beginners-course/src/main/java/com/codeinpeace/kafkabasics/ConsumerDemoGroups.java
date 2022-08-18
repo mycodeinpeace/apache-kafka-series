ef0b5defee0d604fbc6ca96508f39bbfe4f15277
mycodeinpeace <me@codeinpeace.com>
2022-08-18 21:06:49 +0300

    kafka-beginners-course - Java Consumer inside Consumer Group

---
+++ kafka-beginners-course/src/main/java/com/codeinpeace/kafkabasics/ConsumerDemoGroups.java
@@ -0,0 +1,50 @@
+package com.codeinpeace.kafkabasics;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemoGroups {
+
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+    private static final String GROUP_ID = "my-fifth-application";
+    private static final String TOPIC = "first_topic";
+
+    public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class);
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // subscribe consumer to our topic(s)
+        consumer.subscribe(Collections.singleton(TOPIC)); // Collections.singleton means we are only subscribing to one topic
+        // consumer.subscribe(Arrays.asList("first_topic", "second_topic")); // To subscribe multiple topics use Arrays.asList
+
+        // poll for new data
+        while(true) { // TODO: This is bad programming.
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100)); // Timeout is 100 ms
+
+            // Look at the records
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+    }
+}
