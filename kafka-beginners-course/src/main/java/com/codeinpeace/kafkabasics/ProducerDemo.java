12b787019d12de62cd88d92cf1471d88be361a59
mycodeinpeace <me@codeinpeace.com>
2022-08-18 18:37:10 +0300

    kafka-beginners-course - Java Producer

---
+++ kafka-beginners-course/src/main/java/com/codeinpeace/kafkabasics/ProducerDemo.java
@@ -0,0 +1,34 @@
+package com.codeinpeace.kafkabasics;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+
+    public static void main( String[] args ) {
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer kafkaProducer = new KafkaProducer<String, String>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "some message");
+
+        // send data - async
+        kafkaProducer.send(record);
+
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+}
+
