0a3d181bfa7b7a6ca237375177e1ea282f1b3c67
mycodeinpeace <me@codeinpeace.com>
2022-08-23 16:44:45 +0300

    kafka-connect - Kafka Connect Twitter

---
+++ kafka-connect/run.sh
@@ -0,0 +1,8 @@
+# for Mac-OS
+# connect-standalone.sh connect-standalone.properties twitter.properties
+
+# for linux
+# connect-standalone.sh connect-standalone.properties twitter.properties
+
+# for windows
+connect-standalone.sh connect-standalone.properties twitter.properties
